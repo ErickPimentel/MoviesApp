0492048561e3d7bf5411a054952a73d6dd3a91f6
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2024-01-16 10:18:01 -0300

    feat: Presentation layer

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -10,8 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.moviesapp.movieList.presentation.MovieListViewModel
 import com.example.moviesapp.ui.theme.MoviesAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val movieListViewModel = hiltViewModel<MovieListViewModel>()
                     Greeting("Android")
                 }
             }
